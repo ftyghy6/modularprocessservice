2c71383a77fa386d116645ee0f38983010fde5ef
ftyghy6 <gs1pepefoxy@hotmail.com>
2024-05-14 10:16:38 +0700

    feat project

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function findRedundantConnection(edges) {
-  const parent = new Array(edges.length + 1).fill(-1);
-  for (const edge of edges) {
-    const u = find(parent, edge[0]);
-    const v = find(parent, edge[1]);
-    if (u === v) return edge;
-    parent[u] = v;
-  }
-  return [];
+function add(x, y) {
+  return x + y;
 }
-function find(parent, i) {
-  if (parent[i] === -1) return i;
-  return find(parent, parent[i]);
-}
+
+const result = add(5, 3);
+console.log(result); // 8
